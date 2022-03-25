13e34b9f2014a50fded5c2f705b4899c338a620c
Thiago Rizzo <rizzo.17@outlook.com>
2022-03-25 03:55:21 -0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,62 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Flutter Demo', home: MyStateWidgets());
+    return const MaterialApp(title: 'UNORP', home: Bixo());
   }
 }
 
-class MyStateWidgets extends StatelessWidget {
-  const MyStateWidgets({Key? key}) : super(key: key);
+class Bixo extends StatefulWidget {
+  const Bixo({Key? key}) : super(key: key);
 
+  @override
+  State<Bixo> createState() => _BixoState();
+}
+
+class _BixoState extends State<Bixo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            leading: Builder(builder: (BuildContext context) {
-              return BackButton(
-                color: Colors.white,
-                onPressed: () {},
-              );
-            }),
-            title: const Text('Voltar')));
+      appBar: AppBar(
+        leading: IconButton(
+          icon: BackButton(color: Colors.white),
+          onPressed: () {},
+          ),
+          title: const Text("Titulo")),
+      body: Table(
+          border: TableBorder.all(
+            borderRadius: BorderRadius.circular(50)),
+          children: const [TableRow(
+              children: <Widget> [
+                CriaCampo(),
+                CriaCampo(),
+                CriaCampo()]
+              )]
+      )
+    );
   }
+}
+
+class CriaCampo extends StatefulWidget {
+  const CriaCampo({Key? key}) : super(key: key);
+
+  @override
+  State<CriaCampo> createState() => _CriaCampoState();
+}
+
+class _CriaCampoState extends State<CriaCampo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.only(left: 5),
+      child: const TextField(
+          textAlign: TextAlign.center,
+          autofocus: true,
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 25)
+      ),
+    );
+    }
 }
