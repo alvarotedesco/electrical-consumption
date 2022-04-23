93b8b37084e169d244075cd81342ae907ca1da0b
Thiago Rizzo <rizzo.17@outlook.com>
2022-04-23 12:51:24 -0300

    correções de erros

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'API.dart';
 import 'SignUp.dart';
+import 'Demonstration.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
   static const String email = 'E-mail';
   static const String password = 'Senha';
   static const String btSignIn = 'Login';
@@ -11,138 +10,77 @@ class LoginPage extends StatefulWidget {
   static const String btDemonstration = 'Demonstração';
   static const String noAccount = 'Não tem uma conta?';
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  String url = "";
-  late String data;
-  String Querytext = 'Não tem uma cnta?';
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/imges.png'), fit: BoxFit.cover)),
-      padding: const EdgeInsets.only(top: 60, left: 40, right: 40),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: ListView(
-          children: <Widget>[
-            const SizedBox(height: 20),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: "E-mail",
-                focusColor: Colors.amber,
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/imges.png'), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Container(
+            padding: const EdgeInsets.only(top: 60, left: 40, right: 40),
+            child: ListView(
+              children: <Widget>[
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              style: const TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-              style: const TextStyle(color: Colors.grey, fontSize: 20),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              height: 60,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.3, 1],
-                  colors: [
-                    Color.fromARGB(255, 50, 15, 110),
-                    Colors.deepPurple,
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-              ),
-              child: SizedBox(
-                height: 60,
-                child: TextButton(
-                  child: Row(
-                    children: const <Widget>[
-                      Expanded(
-                          child: Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.center,
-                      )),
-                    ],
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: email,
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
                   ),
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => LogonPage(),
-                    //   ),
-                    // );
-                  },
+                  style: const TextStyle(color: Colors.grey, fontSize: 20),
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            SizedBox(
-                height: 20,
-                child: Text(
-                  Querytext,
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
-                )),
-            Container(
-                margin: const EdgeInsets.only(top: 5),
-                height: 60,
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [0.3, 1],
-                    colors: [
-                      Color.fromARGB(255, 50, 15, 110),
-                      Colors.deepPurple,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
-                  ),
+                const SizedBox(
+                  height: 10,
                 ),
-                child: SizedBox(
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: password,
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.grey, fontSize: 20),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
                   height: 60,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      stops: [0.3, 1],
+                      colors: [
+                        Color.fromARGB(255, 50, 15, 110),
+                        Colors.deepPurple,
+                      ],
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
                   child: SizedBox(
+                    height: 60,
                     child: TextButton(
-                        child: Row(children: const <Widget>[
+                      child: Row(
+                        children: const <Widget>[
                           Expanded(
                               child: Text(
-                            "Cadastre-se",
+                            btSignIn,
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                               color: Colors.white,
@@ -150,72 +88,111 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                             textAlign: TextAlign.center,
                           )),
-                        ]),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SignUpPage(),
-                            ),
-                          );
-                        }),
-                  ),
-                )),
-            Container(
-                margin: const EdgeInsets.only(top: 20),
-                height: 60,
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [0.3, 1],
-                    colors: [
-                      Color.fromARGB(255, 50, 15, 110),
-                      Colors.deepPurple,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(15),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
                   ),
                 ),
-                child: SizedBox(
-                  height: 60,
-                  child: SizedBox(
-                    child: TextButton(
-                        child: Row(children: <Widget>[
-                          Expanded(
-                              child: Text(
-                            "Demonstracao",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontSize: 20,
-                            ),
-                            textAlign: TextAlign.center,
-                          )),
-                        ]),
-                        onPressed: () async {
-                          data = await getData("url");
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) => SignupPage(),
-                          //   ),
-                          // );
-                        }),
-                  ),
-                )),
-            SizedBox(
-                height: 20,
-                child: Text(
-                  Querytext,
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
-                )),
-          ],
-        ),
-      ),
-    );
+                const SizedBox(
+                  height: 80,
+                ),
+                const SizedBox(
+                    height: 20,
+                    child: Text(
+                      'Nao tem uma conta?',
+                      style: TextStyle(color: Colors.white),
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(top: 5),
+                    height: 60,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        stops: [0.3, 1],
+                        colors: [
+                          Color.fromARGB(255, 50, 15, 110),
+                          Colors.deepPurple,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 60,
+                      child: SizedBox(
+                        child: TextButton(
+                            child: Row(children: const <Widget>[
+                              Expanded(
+                                  child: Text(
+                                btSignUp,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                                textAlign: TextAlign.center,
+                              )),
+                            ]),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SignUpPage(),
+                                ),
+                              );
+                            }),
+                      ),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    height: 60,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        stops: [0.3, 1],
+                        colors: [
+                          Color.fromARGB(255, 50, 15, 110),
+                          Colors.deepPurple,
+                        ],
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                    child: SizedBox(
+                      height: 60,
+                      child: SizedBox(
+                        child: TextButton(
+                            child: Row(children: const <Widget>[
+                              Expanded(
+                                  child: Text(
+                                btDemonstration,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                                textAlign: TextAlign.center,
+                              )),
+                            ]),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Demonstration()));
+                            }),
+                      ),
+                    )),
+              ],
+            ),
+          ),
+        ));
   }
 }
