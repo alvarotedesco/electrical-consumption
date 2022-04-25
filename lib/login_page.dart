2fa856ca22bb489b8432caa7877b0369fa1d45d0
Thiago Rizzo <rizzo.17@outlook.com>
2022-04-25 07:15:54 -0300

    up

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,7 +5,7 @@ import 'package:electrical_comsuption/widgets/input_decoration_widget.dart';
 import 'package:electrical_comsuption/widgets/snackbar_widget.dart';
 import 'package:flutter/material.dart';
 import 'sign_up.dart';
-import 'demonstration.dart';
+import 'demonstration_page.dart';
 import 'package:electrical_comsuption/widgets/button_widget.dart';
 import 'themes/luvas.dart';
 
