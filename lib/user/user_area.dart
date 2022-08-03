a486900819d5c24c373abfdf4ef0c0796e5d0af0
Controller Alvaro Tedesco <controler01@coverpay.com.br>
2022-08-03 18:08:09 -0300

    Adjust in user area

--- lib/user/user_area.dart
+++ lib/user/user_area.dart
@@ -3,6 +3,7 @@ import 'package:electrical_comsuption/themes/app_text_styles.dart';
 import 'package:electrical_comsuption/user/user_controller.dart';
 import 'package:electrical_comsuption/widgets/button_widget.dart';
 import 'package:electrical_comsuption/widgets/custom_app_bar.dart';
+import 'package:electrical_comsuption/widgets/floating_button_widget.dart';
 import 'package:electrical_comsuption/widgets/input_decoration_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -21,28 +22,59 @@ class _UserAreaState extends State<UserArea> {
   final TextEditingController controllerInputName = TextEditingController();
   final TextEditingController controllerInputEmail = TextEditingController();
   String error = '';
+  bool selected = false;
 
-  Widget _infoUser(info) {
-    return Container(
-      padding: EdgeInsets.only(left: 20),
-      alignment: Alignment.centerLeft,
-      width: double.infinity,
-      height: 50,
-      decoration: BoxDecoration(
-        color: AppColors.transparent,
-        borderRadius: BorderRadius.all(Radius.circular(80)),
-        border: Border.fromBorderSide(
-          BorderSide(
-            color: AppColors.secondary,
-            width: 2,
+  Widget _infoUser(info, {email = false}) {
+    if (email) {
+      return GestureDetector(
+        onTap: () => {
+          setState(() => {
+                selected = !selected,
+              })
+        },
+        child: Container(
+          padding: EdgeInsets.only(left: 20),
+          alignment: Alignment.centerLeft,
+          width: double.infinity,
+          height: 50,
+          decoration: BoxDecoration(
+            color: AppColors.transparent,
+            borderRadius: BorderRadius.all(Radius.circular(80)),
+            border: Border.fromBorderSide(
+              BorderSide(
+                color: AppColors.secondary,
+                width: 2,
+              ),
+            ),
+          ),
+          child: Text(
+            info,
+            style: AppTextStyles.h1WhiteBold,
           ),
         ),
-      ),
-      child: Text(
-        info,
-        style: AppTextStyles.h1WhiteBold,
-      ),
-    );
+      );
+    } else {
+      return Container(
+        padding: EdgeInsets.only(left: 20),
+        alignment: Alignment.centerLeft,
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: AppColors.transparent,
+          borderRadius: BorderRadius.all(Radius.circular(80)),
+          border: Border.fromBorderSide(
+            BorderSide(
+              color: AppColors.secondary,
+              width: 2,
+            ),
+          ),
+        ),
+        child: Text(
+          info,
+          style: AppTextStyles.h1WhiteBold,
+        ),
+      );
+    }
   }
 
   Widget _errorWidget() {
@@ -77,63 +109,73 @@ class _UserAreaState extends State<UserArea> {
         appBar: CustomAppBar(
           userArea: true,
         ),
-        body: Container(
-          padding: EdgeInsets.all(20),
-          child: controller.state != UserState.success
-              ? _errorWidget()
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: CircleAvatar(
-                        backgroundColor: AppColors.secondary,
-                        radius: 95,
+        floatingActionButton: FloatingCustomButtonWidget(
+          selected: selected,
+          canCreate: false,
+          delete: false,
+          onEditButton: (() => {}),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: controller.state != UserState.success
+                ? _errorWidget()
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
                         child: CircleAvatar(
-                          backgroundImage: AssetImage(Meias.teste),
-                          backgroundColor: AppColors.primary,
-                          radius: 90,
-                          // TODO: Quando nao pegar a imagem / criar função
+                          backgroundColor: AppColors.secondary,
+                          radius: 95,
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage(Meias.teste),
+                            backgroundColor: AppColors.primary,
+                            radius: 90,
+                            // TODO: Quando nao pegar a imagem / criar função
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 40),
-                    Text(
-                      controller.user!.name as String,
-                      style: AppTextStyles.whiteBold28,
-                    ),
-                    SizedBox(height: 40),
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: AppColors.black60,
+                      SizedBox(height: 40),
+                      Text(
+                        controller.user!.name as String,
+                        style: AppTextStyles.whiteBold28,
                       ),
-                      child: Column(
-                        children: [
-                          _infoUser(controller.user!.cpf),
-                          SizedBox(height: 20),
-                          _infoUser(controller.user!.email),
-                          InputDecorationWidget(
-                              textInputType: TextInputType.name,
-                              controller: controllerInputName,
-                              label: 'teste'),
-                          SizedBox(height: 20),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              AppButtonWidget(
-                                onPressed: () => {},
-                                texto: 'Excluir conta',
-                                color: AppColors.darkOrange,
+                      SizedBox(height: 40),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: AppColors.black60,
+                        ),
+                        child: Column(
+                          children: [
+                            _infoUser(controller.user!.cpf),
+                            SizedBox(height: 20),
+                            Visibility(
+                              child: _infoUser(
+                                controller.user!.email,
+                                email: true,
                               ),
-                            ],
-                          ),
-                        ],
+                              visible: selected,
+                            ),
+                            SizedBox(height: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                AppButtonWidget(
+                                  onPressed: () => {},
+                                  texto: 'Excluir conta',
+                                  color: AppColors.darkOrange,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+          ),
         ),
       ),
     );
